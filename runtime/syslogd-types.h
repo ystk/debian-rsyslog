909a38edb19abf915fc1907203c6bf32df192084
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2015-04-14 05:34:57 +0900

    git-debimport rsyslog_8.4.2.orig.tar.gz

--- runtime/syslogd-types.h
+++ runtime/syslogd-types.h
@@ -4,7 +4,7 @@
  *
  * File begun on 2007-07-13 by RGerhards (extracted from syslogd.c)
  *
- * Copyright 2007-2012 Adiscon GmbH.
+ * Copyright 2007-2014 Adiscon GmbH.
  *
  * This file is part of the rsyslog runtime library.
  *
@@ -27,9 +27,6 @@
 
 #include "stringbuf.h"
 #include <sys/param.h>
-#if HAVE_SYSLOG_H
-#include <syslog.h>
-#endif
 
 /* we use RSTRUE/FALSE to prevent name claches with other packages */
 #define RSFALSE 0
